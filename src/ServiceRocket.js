ab0b40bb0416ec42e0d11814dd7eb02dd856f940
Tushar Kumar <tushar.ku@HCL.COM>
2020-05-15 20:14:06 +0530

    SCPI with ? Retry functionality until resp received

--- src/ServiceRocket.js
+++ src/ServiceRocket.js
@@ -1,42 +1,55 @@
 import io from 'socket.io-client';
 
+let retryTimeout = false;
+
 export class Rocket {
 	constructor() {}
 
-	static fire(cmd, sec) {
+	static async fire(cmd, sec) {
 		let socket = io("http://localhost:8472");
-		var socketObj = {
-			address: localStorage.getItem('address'),
-			command: cmd
-		};
+		let retryCount = 0;
+		let retryLimit = 3;
+		let finalData = "";
 
 		if(cmd == "FAKE") cmd = '*IDN?';
-		if(cmd == "FAKE3") sec = 5000;
 		if(JSON.parse(localStorage.getItem('demo'))) sec = 500;
 
-		socket.emit('comm', socketObj);
-		
-		var p1 = new Promise((resolve, reject) => {
-			socket.on('comm', function(resp) {
-				resolve(JSON.parse(resp));
-			});
-			// setTimeout( _ => {
-			// 	resolve("mock server response");
-			// }, 1000)
-		});
+		if(cmd.indexOf("?") > -1) {
+			// retryTimer();
+			while(retryCount < retryLimit) {
+				let result = await ignite(socket, cmd);
+				if(result) {
+					finalData = result;
+					break;
+				} else {
+					retryCount++;
+				}
+			}
+		} else {
+			finalData = await ignite(socket, cmd);
+		}
 
-		// if(cmd.indexOf('?') > -1) sec = 1000;
-		var p2 = new Promise((resolve, reject) => {
-			setTimeout( _ => {
-				resolve('default delay');
-			}, sec);
-		});
-
-		return new Promise( (resolve, reject) => {
-			Promise.all([p1, p2])
-			.then(function(values) {
-				resolve(values[0]);
-			});
-		})
+		return finalData;
 	}
+
+}
+
+function ignite(socket, cmd) {
+	var socketObj = {
+		address: localStorage.getItem('address'),
+		command: cmd
+	};
+	socket.emit('comm', socketObj);
+	return new Promise((resolve, reject) => {
+		socket.on('comm', function(resp) {
+			resolve(JSON.parse(resp));
+		});
+	});
+}
+
+function retryTimer() {
+	retryTimeout = false;
+	setTimeout( _ => {
+		retryTimeout = true;
+	}, 15000)
 }
