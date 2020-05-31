45ef3f26b4f0b26092ffbdacf1259bcaf935aeb8
Tushar Kumar <tushar.ku@HCL.COM>
2020-05-31 16:00:07 +0530

    added fake machine files

---
+++ machine.js
@@ -0,0 +1,71 @@
+var io = require("socket.io").listen(8472);
+console.log('Socket listening on port 8472');
+
+io.on('connect', onConnect);
+
+function onConnect(socket) {
+  io.emit('join');
+  socket.on('comm', function(data) {
+    console.log(">>>", data);
+    if (data.command == "GET_DEVICE_LIST") {
+      getDeviceList().then( resp => {
+        io.emit('comm', JSON.stringify(resp));
+        console.log("<<<", resp);
+      })
+    } else {
+      sendSCPI(data.address, data.command).then( resp => {
+        io.emit('comm', JSON.stringify(resp));
+        console.log("<<<", resp);
+      })
+    }
+  });
+}
+
+function getDeviceList() {
+  return new Promise((resolve, reject) => {
+    setTimeout( _ => {
+      resolve(['USB', 'ETHERNET']);
+    }, 100);
+  });
+}
+
+function sendSCPI(address, query) {
+  return new Promise((resolve, reject) => {
+    setTimeout( _ => {
+      var result = getCmdResponse(query);
+      resolve(result);
+    }, 100);
+  });
+}
+
+function getCmdResponse(query) {
+    var final = "";
+    switch (query) {
+        case "*IDN?":                           final = "\"Anritsu,MW82119B/31/19/850/331,1630010,3.83\""; break;
+        case ":INSTrument:CATalog:FULL?":       final = "\"VNA\"2,\"HI_PM\"10,\"MINIPIM\"46"; break;
+        case ":INSTrument:NSELect?":            final = pickRandom(["46","10","2"]); break;
+        case ":CALibration:PIManalyzer:FULL?":  final = pickRandom(["CAL ON","CAL OFF"]); break;
+        case ":PIManalyzer:MEASure:VALue?":     final = createRandom(); break;
+        case ":PIManalyzer:MEASure:STATus?":    final = pickRandom(["0","1"]); break;
+        case ":PIManalyzer:FREQuency:F1?":      final = pickRandom(["869000000","871000000","871000000"]); break;
+        case ":PIManalyzer:FREQuency:F2?":      final = pickRandom(["894000000","894000000","886660000"]); break;
+        default : "";
+    }
+    return final;
+}
+
+function createRandom() {
+  var a = Math.floor(Math.random()*(999-100+1)+100);
+  var b = a - 20;
+  return ("-"+a+".8, -"+b+".8");
+}
+
+function pickRandom(arr) {
+    var ran = Math.floor(Math.random() * Math.floor(arr.length));
+    return arr[ran];
+}
+
+module.exports = {
+  getDeviceList,
+  sendSCPI
+}
