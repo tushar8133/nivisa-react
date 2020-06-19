4ab21b699eb99b6a6f4d352fb7cb0bdce4efb61c
Tushar Kumar <tushar.ku@HCL.COM>
2020-06-19 13:48:42 +0530

    code refactored if no_connection_found

--- src/ComponentConnection.js
+++ src/ComponentConnection.js
@@ -19,16 +19,18 @@ export class Connection extends React.Component {
     render() {
 
         if (this.state.addresses.length === 0) {
-            return <h3>Loading...</h3>
+            return <div className="noConnection">
+                No connection found!<br /> Please check device connections first,<br /> Then close this application and reload again.
+            </div>
         } else return (
             <main id='connection-page'>
-                <div className="radioContainer">
+                <form name="radioForm" className="radioContainer">
                 {
                     this.state.addresses.map((value, index) => (
-                        <div key={value}><input type="radio" id={'rad' + (index + 1)} name='addrs' onInput={_ => this.radioHandler(value)} /><label htmlFor={'rad' + (index + 1)}>{value}</label></div>
+                        <div key={value}><input type="radio" id={'rad' + (index + 1)} name='addrs' onInput={_ => this.radioHandler(value)} value={value}/><label htmlFor={'rad' + (index + 1)}>{value}</label></div>
                     ))
                 }
-                </div>
+                </form>
                 <div id='command-page'>
                     <button onClick={_ => this.sendCommand('*IDN?')}>*IDN?</button>
                     <span className="spacer" />
@@ -45,8 +47,14 @@ export class Connection extends React.Component {
     getDeviceList() {
         Contra.start(['GET_DEVICE_LIST'])
         .then( data => {
-            this.setState(state => ({ addresses: data[0] }));
-            this.selectDefaultOption(data[0][0]);
+            try {
+                if(data[0][0].length == 0) throw null;
+                this.setState(state => ({ addresses: data[0] }));
+                this.selectDefaultOption(data[0][0]);
+            } catch(e) {
+                data[0] = [];
+                this.setState(state => ({ addresses: data[0] }));
+            }
         })
     }
 
@@ -54,9 +62,15 @@ export class Connection extends React.Component {
         localStorage.setItem("address", val);
     }
 
-    selectDefaultOption(val) {
-        document.getElementById('rad1').checked = true;
-        this.radioHandler(val);
+    selectDefaultOption(firstOption) {
+        try {
+            var cache = localStorage.getItem("address");
+            // document.querySelector('input[name="addrs"][value='+cache+']').checked = true;
+            document.radioForm.addrs.value = cache;
+        } catch(e) {
+            document.getElementById('rad1').checked = true;
+            this.radioHandler(firstOption);
+        }
     }
 
     sendCommand(cmd) {
