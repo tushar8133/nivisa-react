999bece242f05f4702cebd7ad92d803c3a0ffcc2
Tushar Kumar <tushar8133@gmail.com>
2020-07-02 00:31:32 +0530

    updated LEGO commands timer

--- src/ServiceLego.js
+++ src/ServiceLego.js
@@ -61,18 +61,18 @@ export const LEGO = [
 	},
 	{
 		id:':INITiate:PIManalyzer:PVT:ALLPower:CAL',
-		desc: 'Calibrating in progress',
+		desc: 'Calibration in progress',
 		delay: '60'
 	},
 	{
 		id:':INITiate:PIManalyzer:RESidual:CAL',
 		desc: 'Residual PIM Calibration',
-		delay: '10'
+		delay: '20'
 	},
 	{
 		id: ':CALibration:PIManalyzer:FULL?',
 		desc: 'Checking Calibration',
-		delay: '10'
+		delay: '1'
 	},
 	{
 		id: ':SYSTem:PRESet',
@@ -82,12 +82,12 @@ export const LEGO = [
 	{
 		id: ':PIManalyzer:OUTPut:POWer',
 		desc: 'Setting Power',
-		delay: '2'
+		delay: '5'
 	},
 	{
 		id: ':PIManalyzer:TEST:DURation',
 		desc: 'Setting Duration',
-		delay: '2'
+		delay: '5'
 	},
 	{
 		id: ':PIManalyzer:OUTPut:POWer?',
@@ -112,61 +112,31 @@ export const LEGO = [
 	{
 		id: ':PIManalyzer:MEASure:VALue?',
 		desc: 'Checking Measured Values',
-		delay: '10'
+		delay: '3'
 	},
 	{
 		id: ':PIManalyzer:FREQuency:F1?',
 		desc: 'Checking Frequency Tone 1',
-		delay: '10'
+		delay: '1'
 	},
 	{
 		id: ':PIManalyzer:FREQuency:F2?',
 		desc: 'Checking Frequency Tone 2',
-		delay: '10'
+		delay: '1'
 	},
 	{
 		id: ':PIManalyzer:IMD:ORDer 3',
 		desc: 'Setting PIM3 Order',
-		delay: '3'
+		delay: '5'
 	},
 	{
 		id: ':PIManalyzer:IMD:ORDer 5',
 		desc: 'Setting PIM5 Order',
-		delay: '3'
+		delay: '5'
 	},
 	{
 		id: ':PIManalyzer:IMD:ORDer 7',
 		desc: 'Setting PIM7 Order',
-		delay: '3'
-	},
-	{
-		id: 'WAIT',
-		desc: 'Please wait...',
-		delay: '10'
-	},
-	{
-		id: 'FAKE1',
-		desc: 'FAKE1...',
-		delay: '1'
-	},
-	{
-		id: 'FAKE2',
-		desc: 'FAKE2...',
-		delay: '2'
-	},
-	{
-		id: 'FAKE3',
-		desc: 'FAKE3...',
-		delay: '3'
-	},
-	{
-		id: 'FAKE4',
-		desc: 'FAKE4...',
-		delay: '4'
-	},
-	{
-		id: 'FAKE5',
-		desc: 'FAKE5...',
 		delay: '5'
 	}
 ]
